721ab248362ab281c4fb788096ad9ab93cd84003
aarif4 <aarif4@masonlive.gmu.edu>
2020-02-16 01:00:30 -0500

    final commit

--- 1-producer_consumer/tests/test.cpp
+++ 1-producer_consumer/tests/test.cpp
@@ -1,22 +1,46 @@
 #include <gtest/gtest.h>
-#include<Producer.hpp>
-#include<Consumer.hpp>
+#include "main.hpp"
 
-TEST(Producer, Init)
+TEST(Producer_Consumer, 1ThreadIter1_1ThreadIter1)
 {
-    Producer* obj = new Producer();
-    ASSERT_EQ(0, obj->get_public_val());
-    obj->set_public_val(23);
-    ASSERT_EQ(23, obj->get_public_val());
+    Queue queue(2);
+    double sleep = 0;
+    int iter = 1;
+
+    Producer pobj("producer1",&queue,sleep,false);
+    pobj.run(iter);
+    std::string pmsg = pobj.get_last_msg();
+
+    Consumer cobj("consumer1",&queue,sleep,false);
+    cobj.run(iter);
+    std::string cmsg = cobj.get_last_msg();
+
+    ASSERT_STREQ(pmsg.c_str(), cmsg.c_str());
 }
 
-TEST(Consumer, Init)
+TEST(Producer_Consumer, 1ThreadIter1Iter9_2ThreadIter1Iter9)
 {
-    Consumer* obj = new Consumer();
-    ASSERT_EQ(0, obj->get_public_val());
-    obj->set_public_val(25);
-    ASSERT_EQ(25, obj->get_public_val());
+    Queue queue(10);
+    double sleep = 0;
+
+    Producer pobj("producer1",&queue,sleep,false);
+    pobj.run(1);
+    std::string pmsg1 = pobj.get_last_msg();
+    pobj.run(9);
+    std::string pmsg2 = pobj.get_last_msg();
+
+    Consumer cobj1("consumer1",&queue,sleep,false);
+    cobj1.run(1);
+    std::string cmsg1 = cobj1.get_last_msg();
+
+    Consumer cobj2("consumer2",&queue,sleep,false);
+    cobj2.run(9);
+    std::string cmsg2 = cobj2.get_last_msg();
+
+    ASSERT_STREQ(pmsg1.c_str(), cmsg1.c_str());
+    ASSERT_STREQ(pmsg2.c_str(), cmsg2.c_str());
 }
+
 
 int main(int argc, char **argv)
     {
